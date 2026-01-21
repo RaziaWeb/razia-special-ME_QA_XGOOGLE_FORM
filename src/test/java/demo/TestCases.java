6742deb16f82f667649ffebde5db3ff1f094d17c
Razia Begam <razia.special@gmail.com>
2026-01-21 11:57:13 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
+// import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+// import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
@@ -17,19 +17,18 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalTime;
+// import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.logging.Level;
 
-import javax.swing.Action;
-import javax.swing.text.DateFormatter;
-
-// import io.github.bonigarcia.wdm.WebDriverManager;
+// import javax.swing.Action;
+// import javax.swing.text.DateFormatter;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrappers =new Wrappers();
 
     /*
      * TODO: Write your tests here with testng @Test annotation.
@@ -38,82 +37,44 @@ public class TestCases {
      */
     @Test
     public void testCase01() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get(
                 "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
 
         Thread.sleep(4000);
 
-        WebElement name = driver.findElement(By.xpath("(//input[@type='text'])[1]"));
-        // name =
-        // driver.findElement(By.xpath("//input[@type='text']/following-sibling::div"));
-        name.click();
-        name.sendKeys("Crio Learner");
+        wrappers.enterName(driver,"Crio Learner" ); //Enter Name
+      
+        wrappers.entermsgWithTime(driver,"I want to be the best QA Engineer!"); //Enter Msg
 
-        WebElement participateTextArea = driver.findElement(By.xpath("//textarea"));
-        long timeStamp = System.currentTimeMillis();
-        participateTextArea.sendKeys("I want to be the best QA Engineer!" + timeStamp);
+        wrappers.selectExperience(driver,"0 - 2"); //SelectExperience
+       
+       
+        List<String>subjectsTobeSelected=List.of("Java","Selenium","TestNG");//selectSubject
+        wrappers.selectSubjects(driver, subjectsTobeSelected);
 
-        WebElement expRadio = driver.findElement(By.xpath("//div[@class='ulDsOb']/span[text()='0 - 2']"));
-        expRadio.click();
+        
+        wrappers.selectAdress(driver,"Mrs"); //selectAddress
+       
+      
+        wrappers.selectDate(driver,7);  // Date-Calender-select givendays before
 
-        List<WebElement> allSubjects = driver
-                .findElements(By.xpath("//div[@class='ulDsOb']/span[contains(@class,'n5vBHf')]"));
-        for (WebElement Subject : allSubjects) {
-            String subjectName = Subject.getText();
-            if (subjectName.equals("Java") || subjectName.equals("Selenium") || subjectName.equals("TestNG")) {
-                Subject.click();
-            }
-        }
+        wrappers.setTime(driver, "07", "30"); //set givenTime
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        WebElement drpAddressed = wait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='vRMGwf oJeWuf'][text()='Choose']")));
-        drpAddressed.click();
-        Thread.sleep(4000);
-        WebElement addressValue = wait.until(ExpectedConditions
-                .elementToBeClickable(By.xpath("//div[@role='option' and @data-value='Mrs']")));
-        addressValue.click();
- Thread.sleep(4000);
-        // for checking option address
-
-//         WebElement address=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'vRMGwf')]")));
-// System.out.println(address);
-
-        // Date-Calender
-        LocalDate expectedDate = LocalDate.now().minusDays(7);
-        DateTimeFormatter datetimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String formatedDate = expectedDate.format(datetimeFormat);
-
-        WebElement Date = driver.findElement(By.xpath("//input[@type='date']"));
-        Date.sendKeys(formatedDate);
-
-        // time Selection
-        // LocalTime currentTime = LocalTime.now();
-
-        // String hour = currentTime.format(DateTimeFormatter.ofPattern("hh"));
-        // String min = currentTime.format(DateTimeFormatter.ofPattern("mm"));
-        // String ampm = currentTime.format(DateTimeFormatter.ofPattern("a"));
-
-        WebElement Hour_txt = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
-        Hour_txt.sendKeys("07");
-
-        WebElement Minute_txt = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
-        Minute_txt.sendKeys("30");
-
-       System.out.println("Form Successfully filled");
-
+        System.out.println("Form Successfully filled");
         Thread.sleep(2000);
         WebElement Submit_btn = driver.findElement(By.xpath("//span[text()='Submit']"));
-        Submit_btn.click();
+        wrappers.clickOnBtn(driver, Submit_btn);
+  
+
         System.out.println("Form Submitted Successfully");
- Thread.sleep(4000);
+        Thread.sleep(4000);
         WebElement successMsg = wait
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='vHW8K']")));
         String msg = successMsg.getText();
 
         System.out.println(msg);
-        //happy coding 
+   
     }
 
     /*
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,11 +1,16 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,4 +26,101 @@ public class Wrappers {
     {
         System.out.println("Wrappers class ready");
     }
+
+    public void enterName(WebDriver driver,String name)
+    {
+        WebElement nameBox = driver.findElement(By.xpath("(//input[@type='text'])[1]"));
+        clickOnBtn(driver,nameBox);
+        sendInput(driver, nameBox, name);
+      
+    }
+
+    public void entermsgWithTime(WebDriver driver,String msg)
+    {
+        WebElement participateTextArea = driver.findElement(By.xpath("//textarea"));
+        long timeStamp = System.currentTimeMillis();
+        String particateTextBoxValue=msg+timeStamp;
+
+        clickOnBtn(driver,participateTextArea);
+        sendInput(driver, participateTextArea,particateTextBoxValue);
+    }
+    
+
+    public void clickOnBtn(WebDriver driver,WebElement element)
+    { 
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    wait.until(ExpectedConditions.elementToBeClickable(element));
+       element.click();
+    }
+
+    public void sendInput(WebDriver driver,WebElement element,String value)
+    {
+        element.sendKeys(value);
+    }
+
+    public void selectExperience(WebDriver driver, String experience)
+    {
+        WebElement expRadio = driver.findElement(By.xpath("//span[@class='H2Gmcc tyNBNd']//span[contains(text(),'"+experience+"')]"));    
+        clickOnBtn(driver,expRadio);
+    }
+
+    public void selectSubjects(WebDriver driver,List<String>expectedSubjects)
+    {
+            List<WebElement> allSubjects = driver.findElements(By.xpath("//div[@class='ulDsOb']/span[contains(@class,'n5vBHf')]"));
+        for (WebElement Subject : allSubjects) 
+        {
+            String subjectName = Subject.getText();
+            // if (subjectName.equals("Java") || subjectName.equals("Selenium") || subjectName.equals("TestNG"))
+           if(expectedSubjects.contains(subjectName))
+                {
+               clickOnBtn(driver,Subject);
+            }
+        }
+
+    }
+
+
+    public void selectAdress(WebDriver driver,String givenAddress)throws InterruptedException
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement drpAddressed = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='vRMGwf oJeWuf'][text()='Choose']")));
+            clickOnBtn(driver,drpAddressed);
+        Thread.sleep(4000);
+
+       
+        List<WebElement> allAddresses=driver.findElements(By.xpath("//div[contains(@class,'ncFHed')]//div[contains(@class,'OIC90c')]"));
+         for(WebElement address: allAddresses)
+         {
+           String addressValue=address.getText();
+           if(givenAddress.equals(addressValue))
+           {
+            clickOnBtn(driver,address);
+           }
+         }
+        Thread.sleep(4000);
+    }
+
+
+
+    public void selectDate(WebDriver driver,int givenDay)
+    {
+        LocalDate expectedDate = LocalDate.now().minusDays(givenDay);
+        DateTimeFormatter datetimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formatedDate = expectedDate.format(datetimeFormat);
+
+        WebElement Date = driver.findElement(By.xpath("//input[@type='date']"));
+       sendInput(driver, Date, formatedDate);
+    }
+
+    public void setTime(WebDriver driver,String hour,String min)
+    {        
+        WebElement Hour_txt = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+        sendInput(driver, Hour_txt, "07");
+
+        WebElement Minute_txt = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+        sendInput(driver, Minute_txt, "30");
+    }
+
+    
 }
